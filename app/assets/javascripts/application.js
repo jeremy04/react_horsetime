06a2f2b05cd13e1e2b64f306e7485f1e1131d121
Jeremy Lipson <jeremy04@gmail.com>
2017-08-07 21:22:14 -0400

    Added bootstrap + dependencies (tether, jquery)
    @import seems to not work right

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
+//= require jquery/dist/jquery
+//= require tether/dist/js/tether
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
